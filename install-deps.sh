69bfe180828d6d3ada30182ba0256041df1664f6
Deji Akala <dakala@gunner.local>
2016-05-18 22:09:04 +0100

    Install git completion and better prompt

--- install-deps.sh
+++ install-deps.sh
@@ -8,3 +8,9 @@ chmod +x ~/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
 
 # z binary is already referenced from .bash_profile
+
+# Install bash auto-complete support for Git.
+cd /tmp && wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash && sudo cp git-completion.bash /etc/profile.d/git-completion.sh
+
+# Install fancy bash prompt, colourful and with Git status support.
+(cd /tmp && git clone --depth 1 https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
