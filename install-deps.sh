d899bc68571b603a7880043cba3953bcf041874e
Deji Akala <dejiakala@gmail.com>
2019-08-19 05:10:14 +0100

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -2,3 +2,8 @@
 
 # Install fancy bash prompt, colourful and with Git status support.
 (cd /tmp && git clone --depth 1 https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
+
+cd ~ && curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer && composer global require drush/drush:dev-master && composer global require drupal/coder
+
+
+phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer && phpcs --config-set colors 1
