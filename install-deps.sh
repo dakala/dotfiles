9aba84f00a2dc4f34510b4b0851d5b02d001a870
Deji Akala <dejiakala@gmail.com>
2021-03-26 10:38:45 +0000

    Update install-deps.sh
    Disable sexy bash prompt

--- install-deps.sh
+++ install-deps.sh
@@ -1,7 +1,7 @@
 # up to you (me) if you want to run this as a file or copy paste at your leisure
 
 # Install fancy bash prompt, colourful and with Git status support.
-(cd /tmp && git clone --depth 1 https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
+#(cd /tmp && git clone --depth 1 https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
 
 cd ~ && curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer && composer global require drush/drush:dev-master && composer global require drupal/coder
 
